f6375cc7c590dba1d37c76564c6b8d1259277fa6
Jinam Shah <jinams47@gmail.com>
2025-02-25 16:10:28 -0500

    rawtherapee install script

--- scripts/download_rawtherapee.sh
+++ scripts/download_rawtherapee.sh
@@ -1,1 +1,7 @@
-wget https://rawtherapee.com/shared/builds/linux/RawTherapee_5.9.AppImage
+# run as `source download_rawtherapee.sh` instead of `./download_rawtherapee.sh`
+
+wget https://rawtherapee.com/shared/builds/linux/RawTherapee_5.11_release.AppImage
+chmod +x RawTherapee_5.11_release.AppImage
+./RawTherapee_5.11_release.AppImage --appimage-extract
+
+export PATH="$(pwd)/squashfs-root/usr/bin/:$PATH"
