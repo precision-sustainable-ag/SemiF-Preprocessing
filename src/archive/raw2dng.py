8d8bd2401a410d948906461e63744e12ff4263f3
Matthew Kutugata <mattkutugata@gmail.com>
2025-03-17 16:10:06 -0400

    Revert "get ccm and ccm conversion, preprocess batch shell script"
    This reverts commit 8e6ba7c7d5785a8e070483f328b4b8fe2226d5d8.

--- src/archive/raw2dng.py
+++ src/archive/raw2dng.py
@@ -1,5 +1,3 @@
-import os
-import shutil
 import numpy as np
 from pathlib import Path
 import logging
@@ -22,7 +20,6 @@ class RawToDNGConverter:
                  batch_id: str,
                  lts_dir: Path,
                  developed_dng_dir: Path,
-                 ccm9x9: np.array
                  ) -> None:
         """
         Class constructor.
@@ -44,9 +41,6 @@ class RawToDNGConverter:
         self.height = self.dng_tags.ImageLength
         self.width = self.dng_tags.ImageWidth
 
-        self.ccm9x9 = ccm9x9
-
-
     def load_raw_image(self, file_path):
         """
         Load raw data from file into a 16-bit numpy array.
@@ -58,37 +52,17 @@ class RawToDNGConverter:
         log.info(f"Loaded raw image from {file_path.name}")
         return raw_image
 
-    @staticmethod
-    def get_3x3_linear(transformation_matrix: np.ndarray) -> np.ndarray:
-        """
-        Gets the first 3 columns of ccm to get linear rgb relationships.
-        Returns: 3x3 array representing the ccm
-        """
-        return transformation_matrix[:3, :3]
-
-    @staticmethod
-    def compress_to_3x3(transformation_matrix):
-        """
-        Assigns weights to linear, quadratic and cubic rgb relationships.
-        Returns: 3x3 array representing the ccm
-        """
-        # todo: needs calibartion
-        linear_weights = [0.6, 0.3, 0.1]
-        return np.sum([w * transformation_matrix[i * 3:(i + 1) * 3, :3]
-                       for i, w in enumerate(linear_weights)], axis=0)
-
-
-    def format_ccm4pidng(self,ccm_9x9):
-        """Convert 9x9 CCM to PIDNG-compatible 3x3 format"""
-        # either get the linear channels or compress all channels
-        # ccm_3x3 = self.get_3x3_linear(ccm_9x9)
-        ccm_3x3 = self.compress_to_3x3(ccm_9x9)
-        normalized_ccm = []
-        for row in ccm_3x3:
-            row_sum = np.sum(row)
-            normalized_row = [[int((value / row_sum) * 10000), 10000] for value in row]
-            normalized_ccm.extend(normalized_row)
-        return normalized_ccm
+    def format_ccm4pidng(self, ccm):
+        # Not implemented yet
+        ccm1 = []
+        for row in ccm:
+            row_sum = sum(row)
+            normalized_row = [
+                (int((value / row_sum) * 10000),
+                    10000) for
+                value in row]
+            ccm1.extend(normalized_row)
+        return ccm1
 
     def configure_dng_tags(self) -> DNGTags:
         """Set DNG tags for the conversion."""
@@ -126,23 +100,24 @@ class RawToDNGConverter:
         
         # TODO: Implement our own ccm instead of this standard one
         # uncalibrated color matrix, just for demo.
-        ccm1 = [[19549, 10000], [-7877, 10000], [-2582, 10000],
+        ccm1 = [[19549, 10000], [-7877, 10000], [-2582, 10000],	
            [-5724, 10000], [10121, 10000], [1917, 10000],
            [-1267, 10000], [ -110, 10000], [ 6621, 10000]]
-        # ccm1 = self.format_ccm4pidng(self.ccm9x9)
+        
         t.set(Tag.ColorMatrix1, ccm1)
+
         return t
 
     def convert_to_dng(self, raw_image: np.array, dng_tags: DNGTags,
-                       raw_file: Path) -> Path:
+                       raw_file: str) -> str:
         """
         Convert the loaded raw image to DNG format and save to output file.
         Args:
             raw_image (np.array): Raw image
             dng_tags (DNGTags): DNG tags
-            raw_file (Path): raw file path
+            raw_file (str): raw file path
         Returns:
-            Path: Output filename
+            str: Output filename
         """
         if raw_image is None:
             raise ValueError("Raw image data not loaded.")
@@ -163,9 +138,8 @@ class DNGConversionPipeline:
         self.dng_tags_cfg = cfg.dng_tags
         self.batch_id = cfg.batch_id
         self.file_masks = cfg.file_masks
+
         self.local_data_dir = Path(cfg.paths.data_dir)
-        # get calculated 9x9 color correction matrix
-        self.ccm9x9 = self._get_ccm9x9()
 
         self.lts_dir = self._find_lts_dir()
         if not self.lts_dir:
@@ -176,16 +150,6 @@ class DNGConversionPipeline:
         log.info(f"Found {len(self.raw_files)} raw images.")
 
         self._set_developed_output_folder()
-
-    def _get_ccm9x9(self) -> Path:
-        ccm_path = Path(self.cfg.paths.image_development) / "color_matrix" / f"{self.cfg.ccm_name}.npz"
-        os.makedirs(ccm_path.parent, exist_ok=True)
-        if not ccm_path.exists():
-            profiles_backup = self.cfg.paths.img_dev_lts_bkp
-            log.warning(f"Color matrix file not found locally, copying from {profiles_backup}")
-            color_matrix_backup_path = Path(profiles_backup) / "color_matrix" / f"{self.cfg.ccm_name}.npz"
-            shutil.copy(color_matrix_backup_path, ccm_path)
-        return np.load(ccm_path)['matrix']
 
     def _find_lts_dir(self) -> Path:
         """Locate the long-term storage directory."""
@@ -215,7 +179,7 @@ class DNGConversionPipeline:
         args = []
         
         for raw_file in self.raw_files:
-            args.append((self.dng_tags_cfg, self.batch_id, self.lts_dir, raw_file, self.developed_dng_dir, self.ccm9x9))
+            args.append((self.dng_tags_cfg, self.batch_id, self.lts_dir, raw_file, self.developed_dng_dir))
         return args
 
     def run(self, multiproc: bool = False) -> None:
@@ -259,7 +223,7 @@ class DNGConversionPipeline:
             except Exception as e:
                 log.exception(f"Error processing {arg[3]}")
 
-def process_image(dng_tags_cfg, batch_id, lts_dir, raw_file, developed_dng_dir, ccm_9x9):
+def process_image(dng_tags_cfg, batch_id, lts_dir, raw_file, developed_dng_dir):
     """
     Multiprocessing function to convert raw image to DNG format in parallel.
     Args:
@@ -268,14 +232,8 @@ def process_image(dng_tags_cfg, batch_id, lts_dir, raw_file, developed_dng_dir, 
         file_masks (DictConfig): File masks config
         lts_dir (Dict): LTS directory
         raw_file (Path): Raw image to convert
-        ccm_9x9 (np.array): 9x9 of the original color correction matrix
-        has three components: linear, quadratic and cubic rgb relations
-        linear: Basic color channel mixing (cols 1-3)
-        quadratic: Non-linear intensity response (cols 4-6)
-        cubic: High-order non-linearities (cols 7-9)
     """
-
-    raw2dng_conv = RawToDNGConverter(dng_tags_cfg, batch_id, lts_dir, developed_dng_dir, ccm_9x9)
+    raw2dng_conv = RawToDNGConverter(dng_tags_cfg, batch_id, lts_dir, developed_dng_dir)
     log.debug("Initialized raw to DNG converter.")
 
     raw_data = raw2dng_conv.load_raw_image(raw_file)
