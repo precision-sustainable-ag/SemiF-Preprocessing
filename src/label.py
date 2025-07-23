ded8c9c3178398c636d3e35fd8602343952939fd
Matthew Kutugata <mattkutugata@gmail.com>
2025-07-23 14:31:55 -0400

    initial no remap script and cfg setup

--- src/label.py
+++ src/label.py
@@ -8,6 +8,7 @@ from src.tasks.label_utils.merge_overlapping_bboxes import main as merge_overlap
 from src.tasks.label_utils.remap_labels import main as remap_labels
 from src.tasks.label_utils.assign_species import main as assign_species
 from src.tasks.label_utils.assign_rates import main as assign_rates
+from src.tasks.label_utils.no_remap_label import main as no_remap_label
 
 from src.utils.artifact_utils import artifact_updater
 
@@ -20,6 +21,7 @@ TASK_REGISTRY = {
     "remap_labels": artifact_updater("remap_labels")(remap_labels),
     "assign_species": artifact_updater("assign_species")(assign_species),
     "assign_rates": artifact_updater("assign_rates")(assign_rates),
+    "no_remap_label": artifact_updater("no_remap_label")(no_remap_label),
 }
 
 
--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 from dataclasses import dataclass, field
 
@@ -7,58 +7,58 @@ log = logging.getLogger(__name__)
 
 @dataclass
 class BBoxCoordinates:
-    top_left: List[float]
-    top_right: List[float]
-    bottom_left: List[float]
-    bottom_right: List[float]
-    local_centroid: List[float]
-    is_normalized: bool
+    top_left: List[float] = None
+    top_right: List[float] = None
+    bottom_left: List[float] = None
+    bottom_right: List[float] = None
+    local_centroid: List[float] = None
+    is_normalized: Optional[bool] = None
 
 @dataclass
 class GlobalCoordinates:
-    top_left: List[float]
-    top_right: List[float]
-    bottom_left: List[float]
-    bottom_right: List[float]
-    global_centroid: List[float]
-    area_sqm: float
+    top_left: Optional[List[float]] = None
+    top_right: Optional[List[float]] = None
+    bottom_left: Optional[List[float]] = None
+    bottom_right: Optional[List[float]] = None
+    global_centroid: Optional[List[float]] = None
+    area_sqm: Optional[float] = None
 
 @dataclass
 class FOV:
-    height: List[float]
-    width: List[float]
-    top_left_xy: List[float]
-    top_right_xy: List[float]
-    bottom_left_xy: List[float]
-    bottom_right_xy: bool
-    fov_area_cm2: float
-    
+    height: float = None
+    width: float = None
+    top_left_xy: List[float] = None
+    top_right_xy: List[float] = None
+    bottom_left_xy: List[float] = None
+    bottom_right_xy: List[float] = None
+    fov_area_cm2: float = None
+
 @dataclass
 class CameraCoefficients:
-    f: float
-    cx: float
-    cy: float
-    b1: float
-    b2: float
-    k1: float
-    k2: float
-    k3: float
-    k4: float
-    p1: float
-    p2: float
+    f: float = None
+    cx: float = None
+    cy: float = None
+    b1: float = None
+    b2: float = None
+    k1: float = None
+    k2: float = None
+    k3: float = None
+    k4: float = None
+    p1: float = None
+    p2: float = None
 
 @dataclass
 class CameraInfo:
-    aligned: bool
     fov: FOV
-    estimated_xyz: List[float]
-    estimated_yaw: float
-    estimated_pitch: float
-    estimated_roll: float
     camera_coefficients: CameraCoefficients
-    pixel_width: float
-    pixel_height: float
-    focal_length: float
+    aligned: bool = None
+    estimated_xyz: List[float] = None
+    estimated_yaw: float = None
+    estimated_pitch: float = None
+    estimated_roll: float = None
+    pixel_width: float = None
+    pixel_height: float = None
+    focal_length: float = None
 
 @dataclass
 class ExifMeta:
@@ -73,15 +73,14 @@ class ExifMeta:
 
 @dataclass
 class BoundingBox:
-    is_primary: bool
-    cutout_exists: bool
-    bbox_xywh: List[int] # Fullsized bbox coordinates in pixel coordinates
-    category_class_id: int
     cutout_id: str
-    non_target_weed: bool
-    non_target_weed_pred_conf: float
-    
-    local_coordinates: BBoxCoordinates
+    category_class_id: int
+    is_primary: bool = None
+    cutout_exists: bool = None
+    bbox_xywh: List[int] = None
+    non_target_weed: bool = None
+    non_target_weed_pred_conf: float = None
+    local_coordinates: BBoxCoordinates = None
     global_coordinates: GlobalCoordinates = None
     overlapping_cutout_ids: List[str] = field(default_factory=list)
 
@@ -98,9 +97,8 @@ class ImageMetadata:
     camera_info: CameraInfo
     fullres_height: int
     fullres_width: int
-    downscaled_height: int
-    downscaled_width: int
-    
+    downscaled_height: int = None
+    downscaled_width: int = None
     annotations: List[BoundingBox] = field(default_factory=list)
 
     
